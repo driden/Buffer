5617e5c1bfba6b107a87c6d5a032690ddb912708
iloureiro <ignacio.loureiro@endava.com>
2018-10-10 12:03:21 -0300

    meh

---
+++ ProductorConsumidor/Buffer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductorConsumidor
+{
+    public class Buffer<T>
+    {
+        private T[] datos = null;
+        private readonly int N = -1;
+        private int cant, prox_ins, prox_elim = 0;
+
+        // Condiciones
+        private SemaphoreSlim no_lleno; //= new Mutex(false, "no_lleno"); // Espero que no este lleno
+        private SemaphoreSlim no_vacio; // = new Mutex(false, "no_vacio"); // Espero que no este vacio
+
+        public Buffer(int bufferSize)
+        {
+            datos = new T[bufferSize];
+            N = bufferSize;
+            no_lleno = new SemaphoreSlim(1); // Espero que no este lleno
+            no_vacio = new SemaphoreSlim(1); // Espero que no este vacio
+        }
+
+        public void Insertar(T x)
+        {
+            if (cant == N)
+                no_lleno.Wait();
+
+            datos[prox_ins] = x;
+            prox_ins = (prox_ins + 1) % N;
+            cant++;
+            Console.WriteLine("add: {0}", Thread.CurrentThread.Name);
+
+            no_vacio.Release();
+
+        }
+
+        public void Extraer(out T x)
+        {
+            if (cant < 1)
+                no_vacio.Wait();
+
+            x = datos[prox_elim];
+            prox_elim = (prox_elim + 1) % N;
+            cant--;
+            Console.WriteLine("cons: {0}", Thread.CurrentThread.Name);
+
+            no_lleno.Release();
+        }
+    }
+}
---
+++ ProductorConsumidor/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProductorConsumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var todas = new List<Thread>();
+
+            var buffer = new Buffer<string>(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                var t = new Thread(() => buffer.Insertar($"Texto producido por {i}"));
+                t.Name = $"productor{i}";  
+                todas.Add(t);
+
+                t.Start();
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread t = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        string leer;
+                        buffer.Extraer(out leer);
+                        Console.WriteLine(leer); 
+
+                        Thread.Sleep(1000);
+                    }
+                });
+                t.Name = $"consumidor{i}";
+                t.Start();
+                todas.Add(t);
+                
+            }
+
+
+            todas = todas.OrderBy(x => Guid.NewGuid()).ToList();
+
+            // Esperar
+            foreach (var thread in todas)
+            {
+                //if (thread.ThreadState != (ThreadState.Stopped | ThreadState.StopRequested))
+                //    continue;
+                thread.Join();
+            }
+
+            Console.WriteLine("key");
+            Console.ReadLine();
+        }
+    }
+}
